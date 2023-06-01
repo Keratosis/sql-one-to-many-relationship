7522b1a31385b745b2fae3204940712bd48b9b26
braxtonshaffie <braxtonshaffie@gmail.com>
2023-06-01 19:28:53 +0300

    creating user table

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 
 
 import os
-from sqlalchemy  import create_engine
+from sqlalchemy  import (create_engine,Integer,Column,String,ForeignKey)
 from sqlalchemy.ext.declarative import declarative_base
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.dirname(os.path.realpath(__file__)) #points current path of the file
 
-conn = 'sqlite:///'+ os.path.join(BASE_DIR, 'user.db')
+conn = 'sqlite:///'+ os.path.join(BASE_DIR, 'user.db') # connects us  with the database 
 
-engine =create_engine(conn,echo = True)
+engine =create_engine(conn,echo = True) #generate our sql
 
-Base = declarative_base
+Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'users' #name of the table
+    
+    id =Column(Integer, primary_key = True)
+    username = Column(String(30),nullable = False)
+    email = Column(String(30),nullable = True)
+    
+    
+    #to return string represetation of the objct create from the above class
+    def __repr__(self):
+        return f"<User('{self.username}','{self.email}')>" 
+    
+
+    
