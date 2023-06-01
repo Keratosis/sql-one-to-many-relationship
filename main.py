7862b553ed923ec2c6bed8430feadc4187f0f160
braxtonshaffie <braxtonshaffie@gmail.com>
2023-06-01 20:10:48 +0300

    binding session to the engine

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 BASE_DIR = os.path.dirname(os.path.realpath(__file__)) #points current path of the file
 
 conn = 'sqlite:///'+ os.path.join(BASE_DIR, 'user.db') # connects us  with the database 
-from sqlalchemy.orm  import relationship # function from the SQLAlchemy library used to define relationships between database tables.
+from sqlalchemy.orm  import relationship,sessionmaker # function from the SQLAlchemy library used to define relationships between database tables.
 
 engine =create_engine(conn,echo = True) #generate our sql
 
@@ -23,7 +23,7 @@ class User(Base):
     post =relationship('post',backref = 'auther')  #first-argument is the name of the table (or model) that the relationship is being defined with 
     
     
-    #to return string represetation of the objct create from the above class
+    #to return string represetation of the object create from the above class
     def __repr__(self):
         return f"<User('{self.username}','{self.email}')>" 
     
@@ -39,6 +39,10 @@ class Post(Base):
         return f"<Post('{self.title}','{self.content}')>"
     
     
-Base.metadata.create_all(engine) #creating    
+Base.metadata.create_all(engine) #creating  the database
+
+session = sessionmaker()(bind = engine)  
+# allows us to create ,read , delete delete ,flash changes  onto our db
+#The session will use this engine to communicate with the database.
 
     
