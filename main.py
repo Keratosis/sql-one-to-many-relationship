d7e86644fd53450c7b1c0342033052a103f95dc0
braxtonshaffie <braxtonshaffie@gmail.com>
2023-06-01 19:51:59 +0300

    done .schema table

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 BASE_DIR = os.path.dirname(os.path.realpath(__file__)) #points current path of the file
 
 conn = 'sqlite:///'+ os.path.join(BASE_DIR, 'user.db') # connects us  with the database 
+from sqlalchemy.orm  import relationship # function from the SQLAlchemy library used to define relationships between database tables.
 
 engine =create_engine(conn,echo = True) #generate our sql
 
@@ -15,14 +16,29 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users' #name of the table
     
-    id =Column(Integer, primary_key = True)
+    id =Column(Integer(), primary_key = True)
     username = Column(String(30),nullable = False)
     email = Column(String(30),nullable = True)
+    
+    post =relationship('post',backref = 'auther')  #first-argument is the name of the table (or model) that the relationship is being defined with 
     
     
     #to return string represetation of the objct create from the above class
     def __repr__(self):
         return f"<User('{self.username}','{self.email}')>" 
     
+class Post(Base):
+    __tablename__ = 'posts'
+    
+    id = Column(Integer, primary_key =True)
+    title  = Column(String(20),nullable =False)
+    content = Column(String(240),nullable =False )
+    user_id =Column(Integer(),ForeignKey("users.id"))
+    
+    def __repr__(self):
+        return f"<Post('{self.title}','{self.content}')>"
+    
+    
+Base.metadata.create_all(engine) #creating    
 
     
